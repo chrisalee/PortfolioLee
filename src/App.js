372b1f10bc3cd0b967fca8b629d4e9ab2e8c8a9b
chris <chrisalee222@gmail.com>
2023-05-11 12:43:19 -0500

    change pathnames in app.js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,33 @@
-import React, { Suspense, lazy } from "react";
+import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Nav from "./components/Nav";
-// import Home from "./screens/Home";
-// import Footer from "./components/Footer";
-// import ScrollTop from "./components/ScrollTop";
-// import Project from "./screens/Project";
+import Home from "./screens/Home";
+import Footer from "./components/Footer";
+import ScrollTop from "./components/ScrollTop";
+import Project from "./screens/Project";
 
 // const Nav = lazy(() => import('./components/Nav'));
-const Project = lazy(() => import('./screens/Project'));
-const Home = lazy(() => import('./screens/Home'));
-const ScrollTop = lazy(() => import('./components/ScrollTop'));
-const Footer = lazy(() => import('./components/Footer'));
-
+// const Project = lazy(() => import('./screens/Project'));
+// const Home = lazy(() => import('./screens/Home'));
+// const ScrollTop = lazy(() => import('./components/ScrollTop'));
+// const Footer = lazy(() => import('./components/Footer'));
 
 const App = () => {
-
   return (
-    <BrowserRouter basename={'/'}>
+    <BrowserRouter basename={"/"}>
       <div className="app">
         <Nav />
-        <Routes >
-          <Route path="/PortfolioLee/portfolio/:id" element={<Suspense fallback={<div>loading...</div>}><Project /></Suspense>} />
-          <Route path="/PortfolioLee/" element={<Suspense fallback={<div>loading...</div>}><Home /></Suspense>} />
-          <Route path="/PortfolioLee/" element={<Home />} exact />
+        <Routes>
+          <Route path="/PortfolioLee/portfolio/:id" element={<Project />} />
+          <Route path="/PortfolioLee" element={<Home />} />
+          <Route path="/PortfolioLee" element={<Home />} exact />
         </Routes>
-        <Suspense fallback={<div>loading...</div>}>
-          <ScrollTop />
-        </Suspense>
-        <Suspense fallback={<div>loading...</div>}>
-          <Footer />
-        </Suspense>
+        <ScrollTop />
+        <Footer />
       </div>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
